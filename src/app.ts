815fd188b70c2f26f6ddb1c423a604a8e02dae19
nxxc <nomad.nxxc@gmail.com>
2021-02-18 16:09:48 +0900

    Add Image Component

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
-import { PageComponents } from './componets/page.js';
+import { ImageComponent } from './componets/page/item/image.js';
+import { PageComponents } from './componets/page/page.js';
 
 class App {
   private readonly page: PageComponents;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponents();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      'Image Title',
+      'https://picsum.photos/600/300'
+    );
+
+    image.attachTo(appRoot, 'beforeend');
   }
 }
 
