87fc0547abd82bdd618aad1851607473ac76899f
nxxc <nomad.nxxc@gmail.com>
2021-02-24 13:39:11 +0900

    style: styling app

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -1,21 +1,15 @@
-import { BaseComponent } from '../../component.js';
-
+import { BaseComponent } from './../../component.js';
 export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
-            <div class="video__player">
-              <iframe class="video__iframe"></iframe>
-            </div>
-            <h3 class="video__title"></h3>
-          </section>`);
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="page-item__title video__title"></h3>
+        </section>`);
 
     const iframe = this.element.querySelector(
       '.video__iframe'
     )! as HTMLIFrameElement;
-
     iframe.src = this.convertToEmbeddedURL(url);
-    console.log(url);
-    //url => video Id 추출
 
     const titleElement = this.element.querySelector(
       '.video__title'
@@ -23,6 +17,13 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     titleElement.textContent = title;
   }
 
+  // input
+  // https://www.youtube.com/watch?v=K3-jG52XwuQ
+  // https://youtu.be/K3-jG52XwuQ
+  // output
+  // https://www.youtube.com/embed/K3-jG52XwuQ
+  // 정규표현식 Regex
+  // https://regexr.com/5l6nr
   private convertToEmbeddedURL(url: string): string {
     const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
     const match = url.match(regExp);
@@ -33,12 +34,3 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     return url;
   }
 }
-
-// <iframe
-//   width='1440'
-//   height='595'
-//   src=
-//   frameborder='0'
-//   allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-//   allowfullscreen
-// ></iframe>;
